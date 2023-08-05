68ceed8fd117ec1028164e3e7d5311ee8dc7e9cf
Kateryna Verveda <katushka211@gmail.com>
2023-08-05 18:38:26 +0200

    add globalErrorHandler

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+
 const { tasksRouter } = require("./routes/tasks");
+
+const { globalErrorHandler } = require("./middlewares/globalErrorHandler");
+const { notFoundHandler } = require("./middlewares/notFoundHandler");
 
 const app = express();
 
-module.exports = app;
-
 app.use("/tasks", tasksRouter);
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
+
+module.exports = app;
